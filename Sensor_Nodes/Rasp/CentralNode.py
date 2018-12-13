b5d54f0cc0f923fb7457ce11d7417af337697a61
Lucas Esteves <lucas.esteves.rocha@gmail.com>
2018-12-13 08:55:38 -0300

    Checkpoint: Communication with thingspeak - ok

--- Sensor_Nodes/Rasp/CentralNode.py
+++ Sensor_Nodes/Rasp/CentralNode.py
@@ -2,5 +2,31 @@ import GsmClient as gsm
 import PubSubClient as mqtt
 
 SerialAT = gsm.GsmClient('/dev/ttyAMA0', 57600)
+SerialAT.restart()
 SerialAT.waitForNetwork()
-SerialAT.gprsConnect("timbrasil.br","tim","tim")
+SerialAT.gprsConnect("claro.com.br","claro","claro")
+SerialAT.sendAT("+CSQ")
+SerialAT.waitResponse()
+SerialAT.sendAT("+HTTPINIT")
+SerialAT.waitResponse()
+SerialAT.sendAT("+HTTPPARA=\"CID\",1")
+SerialAT.waitResponse()
+SerialAT.sendAT("+SAPBR=2,1")
+SerialAT.waitResponse()
+SerialAT.sendAT("+SAPBR=4,1")
+SerialAT.waitResponse()
+SerialAT.sendAT("+CGATT?")
+SerialAT.waitResponse()
+SerialAT.sendAT("+HTTPPARA=\"URL\",\"api.thingspeak.com/update?api_key=X1H7B6RD67MHVGIZ&field1=99773\"")
+SerialAT.waitResponse()
+SerialAT.sendAT("+HTTPACTION=0")
+SerialAT.waitResponse()
+SerialAT.sendAT("+HTTPREAD")
+SerialAT.waitResponse()
+SerialAT.sendAT("+HTTPTERM")
+SerialAT.waitResponse()
+SerialAT.sendAT("+SAPBR=0,1")
+SerialAT.waitResponse()
+
+
+
