47acf225b9aa83efce91e7acfb81f4a12a541c3f
Lucas Esteves Rocha <lucas.esteves.rocha@gmail.com>
2018-12-13 11:38:37 -0200

    Fixing gprsConnect()

--- Sensor_Nodes/Rasp/CentralNode.py
+++ Sensor_Nodes/Rasp/CentralNode.py
@@ -7,7 +7,7 @@ mqtt = MQTT.PubSubClient(SerialAT)
 SerialAT.restart()
 SerialAT.waitForNetwork()
 SerialAT.gprsConnect("claro.com.br","claro","claro")
-
+print("Starting MQTT")
 mqtt.setServer("200.129.43.208",1883)
 mqtt.connect("CentralNode", "teste@teste", "123456")
 mqtt.publish("sensors/coleta_data", "1, 25.0, 25.0, 12.0, 12.0, 10.0, 20181213102500")
